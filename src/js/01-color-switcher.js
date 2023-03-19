5021ce66a8cee5d98d2f7f3055d27b855f2854ff
annkllye <annkllye@gmail.com>
2023-03-19 21:53:22 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,9 +2,11 @@ const body = document.querySelector('body')
 const stopBtn = document.querySelector('[data-stop]');
 const startBtn = document.querySelector('[data-start]');
 
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
 
 startBtn.addEventListener('click', () => {
     timerBgId = setInterval(() => {
@@ -14,11 +16,15 @@ startBtn.addEventListener('click', () => {
     stopBtn.removeAttribute('disabled', true);
 });
 
+
+
 stopBtn.addEventListener('click', () => {
     clearInterval(timerBgId);
     stopBtn.setAttribute('disabled', true);
     startBtn.removeAttribute('disabled', true);
 });
+
+
 
 stopBtn.style.cursor = "pointer";
 startBtn.style.cursor = "pointer";
